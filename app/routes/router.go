a05f8b60b4e87adb63650b807757caa5f920c6be
wjj <jianjun.wu@sunlight-tech.com>
2017-12-04 21:10:14 +0800

    update router.go

--- app/routes/router.go
+++ app/routes/router.go
@@ -6,11 +6,6 @@ import (
 
 func InitRoute(server *dotweb.HttpServer) {
 
-	server.GET("/index", func(ctx dotweb.Context) error {
-		_, err := ctx.WriteString("welcome to my first web!")
-		return err
-	})
-
 	api := server.Group("/gohome/api")
 
 	api.GET("/", func(ctx dotweb.Context) error {
